68f628121738aa53a13688a6d407693541a618bd
Prasetya Ikra Priyadi <prasetya.ikrapriyadi@gmail.com>
2022-02-16 17:30:48 +0700

    Keeper App Part 2

--- src/components/Keeper.jsx
+++ src/components/Keeper.jsx
@@ -3,44 +3,22 @@ import "../css/styles.css";
 import Heading from "./Heading.jsx";
 import Footer from "./Footer.jsx";
 import Card from "./Card.jsx";
+import notes from "./dbnotes.js"
 
-function showCards() {
-  const notes = [
-    {
-      id: "1",
-      title: "Day 1 - Marvel Meetup",
-      msg: "Prepare for annual Marvel Meeting on ComicCon"
-    },
-    {
-      id: "2",
-      title: "Day 2 - Doctor Strange 2",
-      msg: "Watching Doctor Strange 2 Multiverse of Madness"
-    },
-    {
-      id: "3",
-      title: "Day 3 - Doctor Strange 2",
-      msg: "Watching Doctor Strange 2 Multiverse of Madness"
-    },
-    {
-      id: "4",
-      title: "Day 4 - Doctor Strange 2",
-      msg: "Watching Doctor Strange 2 Multiverse of Madness"
-    }
-  ];
-  let cards = [];
-  notes.forEach(card => {
-    cards.push(<Card title={card.title} msg={card.msg} />);
-  });
-  return cards
-}
-
+const noteCard = notes.map(noteinfo => 
+  <Card 
+    key={noteinfo.id}
+    title={noteinfo.title} 
+    msg={noteinfo.msg} 
+  />
+  );
 function App(){
   return (
     <div>
       <Heading />
       <div className="main-container">
         <div className="note-container">
-          {showCards()}
+          {noteCard}
         </div>
       </div>
       <Footer />
---
+++ src/components/dbnotes.js
@@ -0,0 +1,29 @@
+const notes = [
+  {
+    id: "1",
+    title: "Day 1 - Marvel Meetup",
+    msg: "Prepare for annual Marvel Meeting on ComicCon"
+  },
+  {
+    id: "2",
+    title: "Day 2 - Doctor Strange 2",
+    msg: "Watching Doctor Strange 2 Multiverse of Madness"
+  },
+  {
+    id: "3",
+    title: "Day 3 - Doctor Strange 2",
+    msg: "Watching Doctor Strange 2 Multiverse of Madness"
+  },
+  {
+    id: "4",
+    title: "Day 4 - Doctor Strange 2",
+    msg: "Watching Doctor Strange 2 Multiverse of Madness"
+  },
+  {
+    id: "5",
+    title: "Day 5 - Doctor Strange 2",
+    msg: "Watching Doctor Strange 2 Multiverse of Madness"
+  }
+];
+
+export default notes;
